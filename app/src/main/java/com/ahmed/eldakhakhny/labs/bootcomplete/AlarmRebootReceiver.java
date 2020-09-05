ac2db0f5245f44488b1c3eb5152541e27324d92a
Ahmed EL-Dakhakhny <>
2020-09-05 10:13:21 +0200

    starting a flutter activity on booted

--- app/src/main/java/com/ahmed/eldakhakhny/labs/bootcomplete/AlarmRebootReceiver.java
+++ app/src/main/java/com/ahmed/eldakhakhny/labs/bootcomplete/AlarmRebootReceiver.java
@@ -5,12 +5,17 @@ import android.content.Context;
 import android.content.Intent;
 import android.util.Log;
 
+import io.flutter.embedding.android.FlutterActivity;
+
 public class AlarmRebootReceiver extends BroadcastReceiver {
 
     @Override
     public void onReceive(Context oContext, Intent intent) {
         Log.d("AlarmRebootReceiver", "Event Received");
         // 1 you can start a new activity on api 24 devices
-        oContext.startActivity(new Intent(oContext,MainActivity.class));
+        //oContext.startActivity(new Intent(oContext,MainActivity.class));
+
+        // 2
+        oContext.startActivity(FlutterActivity.createDefaultIntent(oContext));
     }
 }
